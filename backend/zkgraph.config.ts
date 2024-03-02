4a7f72d4996322860413890c2f856449329bbb9d
Mingzhe Wang <mingzhe3@illinois.edu>
2024-03-02 05:04:54 -0700

    Delete backend directory

--- backend/zkgraph.config.ts
+++
@@ -1,8 +0,0 @@
-import { UserConfig } from '@hyperoracle/zkgraph-cli'
-
-export default {
-  // Set up your config here
-  UserPrivateKey: '0',
-  // Please set up your pinata JWT
-  PinataJWT: '',
-} as UserConfig
